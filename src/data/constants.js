70feb701b181aaf8ec3e4c1503d1805510eb1dc8
Harshal Birhade <im.harshalb@gmail.com>
2026-03-30 22:41:23 +0530

    Initial commit - complete portfolio source code

---
+++ src/data/constants.js
@@ -0,0 +1,275 @@
+import degree from "../images/degree.png";
+import upgrad from "../images/upgrad.png";
+import shoptrick from "../images/shoptrick.png";
+import silverscreen from "../images/silverscreen.png";
+import newshub from "../images/newshub.png";
+import weatherapp from "../images/weatherapp.png";
+import registration from "../images/registration.png";
+import feedback from "../images/feedback.png";
+
+export const Bio = {
+  name: "Harshal Birhade",
+  roles: [
+    "Full Stack Developer",
+    "Android App Developer",
+    "Front-end developer",
+    // "Back-end developer",
+    "Software Engineer",
+  ],
+  description:
+    "Full Stack Developer with 2+ years of experience building scalable mobile and web applications Specialized in React Native for Android-first mobile solutions and MERN stack development. Proven expertise in end-to-endfeature delivery, offline-first architecture, app releases, and performance optimization. Promoted from Internto Full-time Developer. Passionate about delivering production-ready solutions in fast-paced startup environments.",
+  github: "https://github.com/HarshalBirhade",
+  resume:
+    "https://drive.google.com/file/d/11Nyi4Pfr7IVq8UeP6TKKvNP_IplIsJtS/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/harshal-birhade/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHR_P-7OxbDx2-EpUFbKc24Ee2jKJYms_gUw&s",
+      },
+      {
+        name: "React Native",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHR_P-7OxbDx2-EpUFbKc24Ee2jKJYms_gUw&s",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1024px-Typescript_logo_2020.svg.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Realm",
+        image: "https://avatars.githubusercontent.com/u/7575099?s=200&v=4",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "BitBucket",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAlQk9RfMunNQ8ucGcdizpyBmvCdR64jNZXA&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/android-studio-icon.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://images.seeklogo.com/logo-png/27/2/netlify-logo-png_seeklogo-273744.png",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.testautomatisierung.org/wp-content/uploads/postman-300x183.jpg",
+      },
+      {
+        name: "Firebase",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2Bh8Cgz1HP623XpnVajWfwODcBSa2XC9m8Q&s",
+      },
+      {
+        name: "Google Play Console",
+        image:
+          "https://play-lh.googleusercontent.com/4_RW0mQ5mJhGGJVydTlQsQ0pnqAYq9UoTVm2_gElrgRM13Q02w43HPgYVaMFy4b4smmF",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://play-lh.googleusercontent.com/8G7VFxLNBDW6uhyaGTdSk9Oh6hGZlhjkDpJTUSbJfIV3I8Z_NhYvh82zscnlyWxtOw=w480-h960-rw",
+    role: "Jr. Associate Software Developer",
+    company: "Haqdarshak Empowerment Solutions Pvt Ltd.",
+    date: "April-2024 - current",
+    desc: "Started as an intern and later transitioned into a full-time role, contributing to the development and maintenance of Android mobile applications using React Native, Redux, and Realm. Integrated Firebase services for push notifications, analytics, and crash reporting, while focusing on improving app performance, fixing UI issues, and ensuring a smooth user experience across devices. Actively collaborated with team members in Agile ceremonies, participated in code reviews, and contributed to timely app releases.",
+    skills: [
+      "React Native",
+      "React.js",
+      "Redux",
+      "JavaScript (ES6+)",
+      "HTML5",
+      "CSS3",
+      "Realm (local database)",
+      "Firebase (push notifications, analytics, crash reporting)",
+      "Offline-first app development",
+      "UI/UX improvements and bug fixes",
+      "Performance optimization across devices",
+      "Agile team collaboration",
+      "Code reviews",
+      "Android mobile app development",
+      "Firebase (Crashlytics, Push Notifications, Analytics)",
+      "Git, GitHub",
+      "Postman",
+      "VS Code",
+      "Google Play Console",
+      "npm",
+      "Bitbucket",
+      "Chat GPT",
+      "Claude.ai",
+      "Cursor.ai",
+    ],
+  },
+];
+export const education = [
+  {
+    id: 0,
+    img: upgrad,
+    school: "Upgrad Knowledgehut",
+    degree: "Full Stack Developement Bootcamp",
+    // date: "2023",
+  },
+  {
+    id: 1,
+    img: degree,
+    school: "North Maharashtra University",
+    degree: "Bachelor of Engineering",
+    date: "2020",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "ShopTrick - Ecommerce",
+    description:
+      "Building a full-fledged e-commerce website aiming to eccentric the functionality and user experience of popular e-commerce platforms like Flipkart.",
+    image: shoptrick,
+    tags: [
+      "HTML 5",
+      "CSS 3",
+      "ES-6",
+      "React Js",
+      "Material UI",
+      "Redux",
+      "Eslint",
+      "MongoDb",
+      "Mongoose",
+      "Node Js",
+      "Express Js",
+    ],
+    github: "https://github.com/HarshalBirhade/ShopTrick-Project",
+    webapp: "https://shoptrick.netlify.app/",
+  },
+
+  {
+    id: 2,
+    title: "Feedback UI",
+    description:
+      "Develope a feedback form UI where user write their reviews and provide ratings.",
+    image: feedback,
+    tags: ["HTML 5", "CSS 3", "ES-6", "React Js", "Material UI"],
+    // github: "https://github.com/HarshalBirhade/Haqdarshak-Assignment",
+    webapp: "https://smart-feedback-ui.netlify.app/",
+  },
+
+  {
+    id: 3,
+    title: "SilverScreen",
+    description:
+      "Get the scoop on movies and TV shows. SilverScreen provides release dates, ratings, and genres at your fingertips. Elevate your entertainment experience.",
+    image: silverscreen,
+    tags: ["HTML", "CSS", "JavaScript", "TMDB"],
+    github: "https://github.com/HarshalBirhade/SilverScreen",
+  },
+  {
+    id: 3,
+    title: "NewsHub",
+    description:
+      "Our API based web app stay informed with up-to-the-minute news headlines and articles across categories.",
+    image: newshub,
+    tags: ["HTML", "CSS", "React Js", "News API"],
+    github: "https://github.com/HarshalBirhade/NewsHub",
+  },
+
+  {
+    id: 4,
+    title: "Dynamic Weather App",
+    description:
+      "The Dynamic Weather App is a user-friendly web application designed to provide real-time weather information for any city around the world. Users can simply enter the name of the city they are interested in, and the app fetches the latest weather data using a third-party API.",
+    image: weatherapp,
+    tags: ["HTML", "CSS", "Bootstrap", "HBS-Template Engine"],
+    github: "https://github.com/HarshalBirhade/Weather-App",
+    webapp: "https://harshalbirhade.github.io/Raw-Weather-App/",
+  },
+  {
+    id: 5,
+    title: "User Registration",
+    description: "Develope a frontend for user registration",
+    image: registration,
+    tags: ["HTML 5", "CSS 3", "ES-6", "React Js", "Material UI"],
+    github: "https://github.com/HarshalBirhade/Haqdarshak-Assignment",
+    webapp: "https://haqdarshak-assignment.netlify.app/",
+  },
+];
